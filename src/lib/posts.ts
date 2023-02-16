b06b2fb8ca3fd8f95be911e255c82bb50109deff
5h4ru <64119317+5h4ru@users.noreply.github.com>
2023-02-17 03:06:52 +0900

    done

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,7 +1,8 @@
 import fs from "fs"
 import path from "path"
 import matter from "gray-matter"
-import { remark } from "remark"
+import { unified } from "unified"
+import parser from "remark-parse"
 import remarkGfm from "remark-gfm"
 import remarkHtml from "remark-html"
 
@@ -47,9 +48,10 @@ export const getPostData = async (id: string) => {
   const fileContents = fs.readFileSync(fullPath, "utf8")
   const matterResult = matter(fileContents)
 
-  const processedContent = await remark()
-    .use(remarkHtml)
+  const processedContent = await unified()
+    .use(parser)
     .use(remarkGfm)
+    .use(remarkHtml, { sanitize: false })
     .process(matterResult.content)
   const contentHtml = processedContent.toString();
 
